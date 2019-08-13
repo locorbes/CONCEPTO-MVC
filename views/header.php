9f88858f05a3c4b42abaee5fe138a63ad9857c34
OrbesCarlos <locorbes@hotmail.com>
2019-08-12 21:28:04 -0300

    Added navbar's style.

--- views/header.php
+++ views/header.php
@@ -1,5 +1,6 @@
 <nav id="header">
     <ul>
+        <li><img src="assets/images/nav-logo.svg"></li>
         <li><a href="main">Principal</a></li>
         <li><a href="docs">Documentación</a></li>
         <li><a href="news">Novedades</a></li>
--- views/news/index.php
+++ views/news/index.php
@@ -5,8 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Noticias</title>
+    <?php require "views/head.php";?>
 </head>
-<body>
-    NEWS
+<body>    
+    <?php require "views/header.php";?>
+        <h1>Novedades</h1>
+    <?php require "views/footer.php";?>
 </body>
 </html>
